4ff230d289bad12cbd8d410ab1f1a9d357cf87ff
Mauricio <vivayahoo69@gmail.com>
2023-04-05 09:59:40 -0300

    se sube el proyecto

---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').addEventListener('click', function(){
+    console.log("mi idea fue lograr hacer un boton para los daros personales, otro para estudios y otro para experiencia laboral, pero no logre hacerlo");
+    document.getElementById("H3").innerHTML = ""
+})
